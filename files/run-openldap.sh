a3bba13fa3a227278d9658787014429467bd42b8
yepengxj <yepeng_xj@163.com>
2016-03-29 13:22:41 +0800

    Update run-openldap.sh

--- files/run-openldap.sh
+++ files/run-openldap.sh
@@ -1,26 +1,35 @@
-slapd -h "ldap:/// ldaps:/// ldapi:///" -d stats &
-for ((i=30; i>0; i--))
-do
-    ping_result=`ldapsearch 2>&1 | grep "Can.t contact LDAP server"`
-    if [ -z "$ping_result" ]
-    then
-break
-    fi
-    sleep 1
-done
-if [ $i -eq 0 ]
-then
-    echo "slapd did not start correctly"
-    exit 1
-fi  
+#slapd -h "ldap:/// ldaps:/// ldapi:///" -d stats &
+#for ((i=30; i>0; i--))
+#do
+#    ping_result=`ldapsearch 2>&1 | grep "Can.t contact LDAP server"`
+#    if [ -z "$ping_result" ]
+#    then
+#break
+#    fi
+#    sleep 1
+#done
+#if [ $i -eq 0 ]
+#then
+#    echo "slapd did not start correctly"
+#    exit 1
+#fi  
 
-ldapadd -Y EXTERNAL -H ldapi:/// -f back.ldif &&\
-ldapadd -Y EXTERNAL -H ldapi:/// -f sssvlv_load.ldif &&\
-ldapadd -Y EXTERNAL -H ldapi:/// -f sssvlv_config.ldif &&\
-ldapadd -x -D cn=admin,dc=openstack,dc=org -w password -c -f front.ldif &&\
-ldapadd -x -D cn=admin,dc=openstack,dc=org -w password -c -f more.ldif &&\
-wget -O pass_file $PASS_FILE
-ldapadd -x -D cn=admin,dc=openstack,dc=org -w password -c -f pass_file
+service slapd start ;\
+    cd /ldap &&\
+	ldapadd -Y EXTERNAL -H ldapi:/// -f back.ldif &&\
+	ldapadd -Y EXTERNAL -H ldapi:/// -f sssvlv_load.ldif &&\
+    ldapadd -Y EXTERNAL -H ldapi:/// -f sssvlv_config.ldif &&\
+    ldapadd -x -D cn=admin,dc=openstack,dc=org -w password -c -f front.ldif &&\
+    ldapadd -x -D cn=admin,dc=openstack,dc=org -w password -c -f more.ldif &&\
+    chmod +x /ldap/run-openldap.sh
+
+#ldapadd -Y EXTERNAL -H ldapi:/// -f back.ldif &&\
+#ldapadd -Y EXTERNAL -H ldapi:/// -f sssvlv_load.ldif &&\
+#ldapadd -Y EXTERNAL -H ldapi:/// -f sssvlv_config.ldif &&\
+#ldapadd -x -D cn=admin,dc=openstack,dc=org -w password -c -f front.ldif &&\
+#ldapadd -x -D cn=admin,dc=openstack,dc=org -w password -c -f more.ldif &&\
+#wget -O pass_file $PASS_FILE
+#ldapadd -x -D cn=admin,dc=openstack,dc=org -w password -c -f pass_file
 
 pid=$(ps -A | grep slapd | awk '{print $1}')
 kill -2 $pid || echo $?
